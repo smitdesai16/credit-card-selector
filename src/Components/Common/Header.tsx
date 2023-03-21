a1e237d39f2b211f16229a14bd66b3e90b633cba
Smit Desai <smit.desai16@gmail.com>
2023-03-21 18:16:36 -0500

    Fixing header

--- src/Components/Common/Header.tsx
+++ src/Components/Common/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
 	return (
 		<header className="container">
-			<h1 className="text-center">Hello</h1>
+			<h1 className="text-center">Credit Card Selector</h1>
 		</header>
 	)
 }
