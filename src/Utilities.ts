becf953907bedbf2473a80a1a4967ee51f38c842
Smit Desai <smit.desai16@gmail.com>
2023-03-21 17:04:36 -0500

    Get Unique benefits and sort

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -4,14 +4,38 @@ import { DiscoverCreditCard } from "./DataLayer/Discover";
 import { ICreditCardBenefit } from "./Model/ICreditCardBenefit";
 
 export const getCreditCardBenefits = (): ICreditCardBenefit[] => {
-	let result: ICreditCardBenefit[] = [];
+	let creditCards: ICreditCardBenefit[] = [];
 	const boa = new BankOfAmericaCreditCard(BankOfAmericaPreferredRewards.Platinum);
 	const citi = new CitiCreditCard();
 	const discover = new DiscoverCreditCard();
 	
-	result = result.concat(boa.getCreditCardBenefits());
-	result = result.concat(citi.getCreditCardBenefits());
-	result = result.concat(discover.getCreditCardBenefits());
+	creditCards = creditCards.concat(boa.getCreditCardBenefits());
+	creditCards = creditCards.concat(citi.getCreditCardBenefits());
+	creditCards = creditCards.concat(discover.getCreditCardBenefits());
 	
+	let dictionaryStorage = new Map();
+
+	creditCards.forEach(element => {
+		if(dictionaryStorage.has(element.benefit))
+		{
+			if((dictionaryStorage.get(element.benefit) as ICreditCardBenefit).percentage < element.percentage)
+			{
+				dictionaryStorage.set(element.benefit, element);
+			}
+		}
+		else
+		{
+			dictionaryStorage.set(element.benefit, element);
+		}
+	});
+
+	let result: ICreditCardBenefit[] = [];
+
+	dictionaryStorage.forEach(element => {
+		result.push(element);
+	});
+
+	result.sort((a, b) => b.percentage - a.percentage);
+
 	return result;
 }
